475e90611f629c80319207a2f4784bd6a417c6c2
Bhavya Jha <101202753+bhavyajha17@users.noreply.github.com>
2023-03-29 14:03:13 +0530

    Create CC 303 - Time = Speed x Distance.py

---
+++ CC 303 - Time = Speed x Distance.py
@@ -0,0 +1,31 @@
+import heapq
+
+def dijkstra(graph, start, end):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    queue = [(0, start)]
+    while queue:
+        current_distance, current_node = heapq.heappop(queue)
+        if current_node == end:
+            return current_distance
+        if current_distance > distances[current_node]:
+            continue
+        for neighbour, weight in graph[current_node].items():
+            distance = current_distance + weight
+            if distance < distances[neighbour]:
+                distances[neighbour] = distance
+                heapq.heappush(queue, (distance, neighbour))
+    return -1
+
+n, m, p, q, t = map(int, input().split())
+
+graph = {i: {} for i in range(1, n+1)}
+for _ in range(m):
+    u, v = map(int, input().split())
+    if u == v:
+        continue
+    graph[u][v] = t
+    graph[v][u] = t
+
+result = dijkstra(graph, p, q)
+print(result)
